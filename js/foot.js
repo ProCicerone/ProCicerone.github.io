fb4f5fd777eb7c96f5148a4bf7c3dbcdf6a25710
EnricoMiccoli <EnricoMiccoli@users.noreply.github.com>
2015-12-04 18:20:06 +0100

    Implementa footnotes #30

---
+++ js/foot.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#foot-wrapper').hide();
+    $('.foot-number').data('isOpen', '')
+    $('.foot-number').click(function() {
+        if (! $(this).data('isOpen')) {
+            $(this).data('isOpen', 'yes');
+            $(this).after('<div class="foot-text">' + $('#foot-wrapper').find('#ft-' + $(this).attr('id').substr(3)).html() + '</div>');
+        } else {
+            $(this).data('isOpen', '');
+            // $('.foot-text').remove();
+            $(this).next().remove();
+        }
+    });
+});
